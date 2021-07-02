a1d4653ea2cf225f9c793a377baf68fc9a4248d7
Tanya Y <brulek7@gmail.com>
2021-07-02 19:12:32 -0400

    deploying

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittracker", {
   useFindAndModify: false,
 });
 
-// routes
-// app.use(require("./routes/api.js"));
+routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
