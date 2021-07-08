750c318abff7ba73417e4ecee2e3a88a9445cd92
Tanya Y <brulek7@gmail.com>
2021-07-08 15:15:00 -0400

    updated models

--- models/workouts.js
+++ models/workouts.js
@@ -2,36 +2,46 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutsSchema = new Schema({
-  date: {
-    type: Date,
-    required: true,
-  },
   day: {
-    type: Number,
+    type: Date,
+    default: Date.now,
   },
-
-  exercises: {
-    type: Array,
-    default: undefined,
-    duration: {
-      type: Number,
+  exercises: [
+    {
+      name: {
+        type: String,
+        enum: ["cardio", "resistance"],
+        trim: true,
+        required: true,
+      },
+      type: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      weight: {
+        type: Number,
+        default: 0,
+      },
+      sets: {
+        type: Number,
+        default: 0,
+      },
+      reps: {
+        type: Number,
+        default: 0,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      distance: {
+        type: Number,
+        default: 0,
+      },
     },
-  },
-
-  totalDuration: {
-    type: Number,
-    default: 0,
-  },
+  ],
 });
-
-WorkoutsSchema.methods.getTotalDuration = async function () {
-  this.totalDuration = 0;
-  this.exercises.forEach((element) => {
-    this.totalDuration += Number(element.duration);
-  });
-
-  return Number(this.totalDuration);
-};
 
 const Workouts = mongoose.model("Workouts", WorkoutsSchema);
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,87 +2,65 @@ const router = require('express').Router();
 const mongoose = require("mongoose");
 const Workouts = require("../models/workouts.js");
 
-// router = express();
-var d = new Date();
+//Get all workouts
+router.get('/api/workouts', (req, res) => {
+  Workouts.aggregate([
+    {
+      $addFields: {
+        totalDuration: {$sum: '$exercises.duration'},
+      },
+    },
+  ])
+    .then(workoutData => {
+        res.json(workoutData);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
-// module.exports = function (router) {
-  //Get all workouts
-  router.get("/api/workouts", (req, res) => {
-    Workouts.find({})
-      .sort({ date: 1 })
-      .then((data) => {
-        res.json(data);
+//
+router.get('/api/workouts/range', (req, res) => {
+  Workouts.aggregate([
+    {
+      $addFields: {
+        totalDuration: {$sum: '$exercises.duration'},
+      },
+    },
+  ])
+    .sort({_id: -1})
+    .limit(7)
+    .then(workoutData => {
+      res.json(workoutData);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+ //Create new workout
+router.post('/api/workouts', ({body}, res) => {
+  Workouts.create(body)
+    .then(workoutData => {
+      res.json(workoutData);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//Update workouts
+router.put('/api/workouts/:id', ({body, params}, res) => {
+    Workouts.findByIdAndUpdate(
+      {_id: params.id},
+      {$push: {exercises: body}},
+      {new: true})
+      .then(workoutData => {
+        res.json(workoutData);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch(err => {
+        res.status(400).json(err);
       });
   });
-
-  router.put("/api/workouts/:id", async (req, res) => {
-    console.log("PUT ID", req.params.id);
-    // INCLUDE ALL OF EXERCISE
-    try {
-      var addExcercise = await Workouts.findByIdAndUpdate(
-        { _id: req.params.id },
-        {
-          $set: { day: d.getDay(), date: Date.now() },
-          $push: { exercises: req.body },
-        },
-        { new: true }
-      );
-      addExcercise.getTotalDuration();
-      console.log("update", addExcercise);
-      var addTotalDur = await Workouts.findByIdAndUpdate(
-        { _id: req.params.id },
-        { $set: { totalDuration: addExcercise.totalDuration } },
-        { new: true }
-      );
-      console.log(addTotalDur, "update after");
-      res.json(addTotalDur);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-  //Create new workout
-  router.post("/api/workouts", (req, res) => {
-    Workouts.create({ date: Date.now(), day: d.getDay() })
-      .then((data) => {
-        res.json(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-
-  //Get all workouts
-  router.get("/api/workouts/range", async (req, res) => {
-    var sunday = new Date(
-      new Date().setDate(new Date().getDate() - d.getDay())
-    ).setHours(00, 00, 00);
-
-    try {
-      const data = await Workouts.find({ date: { $gte: sunday } }).sort({
-        date: 1,
-      });
-      console.log(data);
-      res.json(data);
-    } catch (err) {
-      throw err;
-    }
-  });
-
-
-function dayCalc(num) {
-  var d = new Date();
-  num = num % 7;
-  var sub = d.getDay() - num;
-
-  if (sub >= 0) {
-    return sub;
-  } else {
-    sub += 7;
-    return sub;
-  }
-}
 
 module.exports = router;
